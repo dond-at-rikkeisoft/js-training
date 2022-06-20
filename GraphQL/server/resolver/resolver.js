a579ef4801fcc556d7f64e5d65319ab00143c61a
trangpth <hientrang452k@gmail.com>
2022-06-20 17:36:15 +0700

    aaa

--- GraphQL/server/resolver/resolver.js
+++ GraphQL/server/resolver/resolver.js
@@ -9,7 +9,7 @@ const resolvers = {
         authors: async(parent, args, {mongoDataMethods}) =>await mongoDataMethods.getAllAuthors(),
         author: async(parent, {id}, {mongoDataMethods}) => await mongoDataMethods.getAuthorById(id),
     },
-     
+        
     Book : {
         author: async ({authorId}, args, {mongoDataMethods}) => await mongoDataMethods.getAuthorById(authorId)
     },
